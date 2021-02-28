2f5713db819b60f6f5f8054381b4ba8da9e0cd78
Poornima <sridhar.poornima18@gmail.com>
2021-02-28 14:55:23 +0530

    created project with create react app and added files for home page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import HomePage from './homepage.component';
+import './homepage.styles.scss'
+import './directory.styles.scss'
+import './menu-item.styles.scss'
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
